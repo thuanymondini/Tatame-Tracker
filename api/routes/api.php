c9e13470e7c7b2fa7da9bffbe6817d25d1d5da29
ThuanyDeSouza <thuanydesouza26@gmail.com>
2026-04-01 16:36:22 -0300

    feat: atualiza as rotas

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TechniqueCategorieController;
+use App\Http\Controllers\TechniqueController;
+use App\Http\Controllers\DailyWorkoutController;
 
 Route::get('/techniqueCategorie', [TechniqueCategorieController::class, 'index']);
 Route::get('/techniqueCategorie/{id}', [TechniqueCategorieController::class, 'show']);
 Route::post('/techniqueCategorie', [TechniqueCategorieController::class, 'store']);
+Route::get('/technique', [TechniqueController::class, 'index']);
+Route::get('/technique/{id}', [TechniqueController::class, 'show']);
+Route::post('/technique', [TechniqueController::class, 'store']);
+Route::put('/technique/{id}', [TechniqueController::class, 'update']);
+Route::delete('/technique/{id}', [TechniqueController::class, 'destroy']);
+Route::get('/dailyWorkouts', [DailyWorkoutController::class, 'index']);
+Route::get('/dailyWorkouts/{id}', [DailyWorkoutController::class, 'show']);
+Route::post('/dailyWorkouts', [DailyWorkoutController::class, 'store']);
