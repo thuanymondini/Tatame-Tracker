7b46920ef8ae69e88320294b73fe2a064423685f
ThuanyDeSouza <thuanydesouza26@gmail.com>
2026-04-06 14:11:54 -0300

    fix: corrige os nomes dos retornos da Technique Resource

--- api/app/Http/Resources/TechniqueResource.php
+++ api/app/Http/Resources/TechniqueResource.php
@@ -9,15 +9,15 @@ class TechniqueResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'id'               => $this->id,
-            'name'             => $this->name,
-            'description'      => $this->description,
-            'category'         => [
-                'id'          => $this->category->id,
-                'name'        => $this->category->name,
-                'description' => $this->category->description,
+            'id_technique'           => $this->id,
+            'name_technique'         => $this->name,
+            'description_technique'  => $this->description,
+            'category'               => [
+                'id_category'             => $this->category->id,
+                'name_category'           => $this->category->name,
+                'description_category'    => $this->category->description,
             ],
-            'linked_technique' => $this->when(
+            'linked_technique'       => $this->when(
                 !is_null($this->linked_technique),
                 fn() => new TechniqueResource($this->linkedTechnique)
             ),
