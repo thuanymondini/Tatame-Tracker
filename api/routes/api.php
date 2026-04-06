698611be8735c6aab7ccb3b522ac8f87dbe73a29
ThuanyDeSouza <thuanydesouza26@gmail.com>
2026-04-06 14:05:47 -0300

    feat: atualiza as rotas e os arquivos de docker

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,13 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TechniqueCategorieController;
+use App\Http\Controllers\TechniqueCategoryController;
 use App\Http\Controllers\TechniqueController;
 use App\Http\Controllers\DailyWorkoutController;
 
-Route::get('/techniqueCategorie', [TechniqueCategorieController::class, 'index']);
-Route::get('/techniqueCategorie/{id}', [TechniqueCategorieController::class, 'show']);
-Route::post('/techniqueCategorie', [TechniqueCategorieController::class, 'store']);
+Route::get('/techniqueCategory', [TechniqueCategoryController::class, 'index']);
+Route::get('/techniqueCategory/{id}', [TechniqueCategoryController::class, 'show']);
+Route::post('/techniqueCategory', [TechniqueCategoryController::class, 'store']);
+Route::match(['put', 'patch'], '/techniqueCategory/{id}', [TechniqueCategoryController::class, 'update']);
+Route::delete('/techniqueCategory/{id}', [TechniqueCategoryController::class, 'destroy']);
 Route::get('/technique', [TechniqueController::class, 'index']);
 Route::get('/technique/{id}', [TechniqueController::class, 'show']);
 Route::post('/technique', [TechniqueController::class, 'store']);
@@ -16,3 +18,5 @@ Route::delete('/technique/{id}', [TechniqueController::class, 'destroy']);
 Route::get('/dailyWorkouts', [DailyWorkoutController::class, 'index']);
 Route::get('/dailyWorkouts/{id}', [DailyWorkoutController::class, 'show']);
 Route::post('/dailyWorkouts', [DailyWorkoutController::class, 'store']);
+Route::match(['put', 'patch'], '/dailyWorkouts/{id}', [DailyWorkoutController::class, 'update']);
+Route::delete('/dailyWorkouts/{id}', [DailyWorkoutController::class, 'destroy']);
