f31ba5b8ac77fe1417781a7c3600ea070bd6d06f
ThuanyDeSouza <thuanydesouza26@gmail.com>
2026-04-14 17:03:29 -0300

    fix: ajusta as rotas do App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Workouts } from '@/pages/Workouts'
+import { DailyWorkouts } from '@/pages/DailyWorkouts'
 import { Techniques } from '@/pages/Techniques'
 import { TechniqueCategories } from '@/pages/TechniqueCategories'
 import { ThemeProvider } from "@/components/theme-provider"
@@ -11,28 +11,10 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<Topbar/>}>
-            <Route path="/" element={<Workouts />} />
+            <Route path="/" element={<DailyWorkouts />} />
             <Route path="/technique" element={<Techniques />} />
             <Route path="/techniqueCategory" element={<TechniqueCategories/>} />
           </Route>
-          {/* <Route path="/" element={<Dashboard />} /> */}
-
-          {/* Categorias */}
-          {/* <Route path="/categories" element={<CategoriesList />} /> */}
-          {/* <Route path="/categories/new" element={<CategoryForm />} />
-          <Route path="/categories/:id" element={<CategoryDetail />} />
-          <Route path="/categories/:id/edit" element={<CategoryForm />} /> */}
-
-          {/* Técnicas */}
-          {/* <Route path="/techniques" element={<TechniquesList />} />
-          <Route path="/techniques/new" element={<TechniqueForm />} />
-          <Route path="/techniques/:id" element={<TechniqueDetail />} />
-          <Route path="/techniques/:id/edit" element={<TechniqueForm />} /> */}
-
-          {/* Treinos */}
-          {/* <Route path="/workouts/new" element={<WorkoutForm />} />
-          <Route path="/workouts/:id" element={<WorkoutDetail />} />
-          <Route path="/workouts/:id/edit" element={<WorkoutForm />} /> */}
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
