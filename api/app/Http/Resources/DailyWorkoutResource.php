ed6f2e127bc869572d95cb2e93a38cc4e64b9c60
ThuanyDeSouza <thuanydesouza26@gmail.com>
2026-04-14 16:55:28 -0300

    fix: ajusta o nome do id_training para id no Resource do treino diário

--- api/app/Http/Resources/DailyWorkoutResource.php
+++ api/app/Http/Resources/DailyWorkoutResource.php
@@ -10,7 +10,7 @@ class DailyWorkoutResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'id_training'      => $this->id,
+            'id'      => $this->id,
             'training_date'    => $this->training_date,
             'observations'     => $this->observations,
             'techniques'       => $this->techniques->map(fn($technique) => new TechniqueResource($technique)),
